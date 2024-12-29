da8a3727488063d578dcb18b7e18339cc9930f8e
Rekos-debug <salvatoregolino20@gmail.com>
2024-12-29 02:57:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿document.getElementById('bottone-sorteggia').addEventListener('click', () => {
+    const argomenti = document.getElementById('input-argomenti').value.split('\n').filter(a => a.trim() !== '');
+    if (argomenti.length === 0) {
+        alert('Inserisci almeno un argomento!');
+        return;
+    }
+    const casuale = argomenti[Math.floor(Math.random() * argomenti.length)];
+    document.getElementById('risultato').innerText = `Argomento sorteggiato: ${casuale}`;
+});
